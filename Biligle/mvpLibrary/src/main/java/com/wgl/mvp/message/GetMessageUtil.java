80abf10c4d226365865c97a0ef512d14866df687
Biligle <2425712136@qq.com>
2016-08-04 09:45:48 +0800

    添加判断读取短信权限

--- Biligle/mvpLibrary/src/main/java/com/wgl/mvp/message/GetMessageUtil.java
+++ Biligle/mvpLibrary/src/main/java/com/wgl/mvp/message/GetMessageUtil.java
@@ -1,10 +1,13 @@
 package com.wgl.mvp.message;
 
+import android.Manifest;
 import android.app.Activity;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Handler;
+
+import com.wgl.mvp.requestPermission.RequestPermissionUtil;
 
 /**
  * Created by wgl.
@@ -28,22 +31,24 @@ import android.os.Handler;
         public void onChange(boolean selfChange) {
             super.onChange(selfChange);
             // 读取收件箱中指定号码的短信(发件箱:"content://sms/outbox")
-            if(null == phone || "".equals(phone)){
-                cursor = activity.managedQuery(Uri.parse("content://sms/inbox"),
-                        null, null,
-                        null, "_id desc");
-            }else{
-                cursor = activity.managedQuery(Uri.parse("content://sms/inbox"),
-                        new String[] { "_id", "address", "read", "body" }, "address=? and read=?",
-                        new String[] {phone, "0" }, "_id desc");
-            }
-            // 按短信id排序，如果按date排序的话，修改手机时间后，读取的短信就不准了
-            if (cursor != null && cursor.getCount() > 0) {
-                cursor.moveToFirst();
-                if (cursor.moveToFirst()) {
-                    String smsbody = cursor
-                            .getString(cursor.getColumnIndex("body"));
-                    iMessage.getMessage(smsbody);
+            if(RequestPermissionUtil.getRequestPermissionUtilInstance().
+                    insertDummyContactWrapper(activity, Manifest.permission.READ_SMS,RequestPermissionUtil.REQUEST_READ_SMS)){
+                if(null == phone || "".equals(phone)){
+                    cursor = activity.managedQuery(Uri.parse("content://sms/inbox"),
+                            null, null,
+                            null, "_id desc");
+                }else{
+                    cursor = activity.managedQuery(Uri.parse("content://sms/inbox"),
+                            new String[] { "_id", "address", "read", "body" }, "address=? and read=?",
+                            new String[] {phone, "0" }, "_id desc");
+                }
+                // 按短信id排序，如果按date排序的话，修改手机时间后，读取的短信就不准了
+                if (cursor != null && cursor.getCount() > 0) {
+                    cursor.moveToFirst();
+                    if (cursor.moveToFirst()) {
+                        String smsbody = cursor
+                                .getString(cursor.getColumnIndex("body"));
+                        iMessage.getMessage(smsbody);
 //                    message = smsbody;
 //                    Toast.makeText(activity,smsbody,Toast.LENGTH_SHORT).show();
 //                if(codeCount == 6){
@@ -63,8 +68,10 @@ import android.os.Handler;
 //                        editText.setText(list.get(0));
 //                    }
 //                }
+                    }
                 }
             }
+
         }
 
 
