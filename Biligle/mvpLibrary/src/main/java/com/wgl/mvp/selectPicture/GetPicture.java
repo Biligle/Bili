fedf9e7b994ab04703affa7c24c61b661c81ce8c
Biligle <2425712136@qq.com>
2016-08-19 16:55:22 +0800

    Update GetPicture.java

--- Biligle/mvpLibrary/src/main/java/com/wgl/mvp/selectPicture/GetPicture.java
+++ Biligle/mvpLibrary/src/main/java/com/wgl/mvp/selectPicture/GetPicture.java
@@ -11,8 +11,10 @@ import android.util.Log;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
@@ -26,8 +28,16 @@ public class GetPicture {
      * 扫描拿到所有的图片路径
      */
     private ArrayList<String> mImageFloders = new ArrayList<String>();
+    /**
+     * 扫描拿到每个文件夹的封面图片路径
+     */
+    private ArrayList<String> firstImages= new ArrayList<String>();
     private Context context;
     private ISelect iSelect;
+    /**
+     * 临时的辅助类，用于防止同一个文件夹的多次扫描
+     */
+    private HashSet<String> mDirPaths = new HashSet<String>();
 
     public GetPicture(Context context, ISelect iSelect){
         this.context = context;
@@ -50,7 +60,6 @@ public class GetPicture {
         new Thread(new Runnable(){
             @Override
             public void run(){
-                String firstImage = null;
                 Uri mImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
                 ContentResolver mContentResolver = context.getContentResolver();
                 // 只查询jpeg和png的图片
@@ -65,15 +74,54 @@ public class GetPicture {
                     String path = mCursor.getString(mCursor
                             .getColumnIndex(MediaStore.Images.Media.DATA));
 //                    Log.e("TAG", path);
-                    mImageFloders.add(path);
-            }
+//                    if(mImageFloders.contains(path)){
+//                        continue;
+//                    }else{
+                        mImageFloders.add(path);
+//                    }
+                    // 获取该图片的父路径名
+                    File parentFile = new File(path).getParentFile();
+                    if (parentFile == null) {
+                        continue;
+                    }
+                    String dirPath = parentFile.getAbsolutePath();
+                    // 利用一个HashSet防止多次扫描同一个文件夹（不加这个判断，图片多起来还是相当恐怖的~~）
+                    if (mDirPaths.contains(dirPath)){
+                        continue;
+                    }else{
+                        mDirPaths.add(dirPath);
+                        firstImages.add(path);
+                    }
+                }
                 mCursor.close();
                 mProgressDialog.dismiss();
                 // 通知Handler扫描图片完成
-                iSelect.getPicture(mImageFloders);
+                iSelect.getPicture(mImageFloders,firstImages);
 
             }
         }).start();
 
+    }
+
+    /**
+     * 获取文件夹中的所有图片
+     * @param path
+     */
+    public static ArrayList<String> getFilePic(String path){
+        String start = path.substring(0,path.lastIndexOf("/"));
+        List<String> paths = Arrays.asList(new File(path).getParentFile().list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String filename) {
+                if(filename.endsWith("jpeg") || filename.endsWith(".jpg") || filename.endsWith(".png")){
+                    return true;
+                }
+                return false;
+            }
+        }));
+        ArrayList<String> data = new ArrayList<String>();
+        for(String str : paths){
+            data.add(start + "/"+str);
+        }
+        return data;
     }
 }
